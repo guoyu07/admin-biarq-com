3540e8ec300156047fcb7e385a3f437500708451
Thesyncim <thesyncim@gmail.com>
2012-07-09 15:04:12 +0100

    test session2

--- controllers/SessionsController.php
+++ controllers/SessionsController.php
@@ -10,28 +10,28 @@ class SessionsController extends \lithium\action\Controller {
 
     public function add() {
 
-
-
         if ($this->request->data) {
 
             /**
-             * login admin 
+             * login admin
              */
-            
-if (($this->request->data['password'] == 'Kirk1zodiak') && ($this->request->data['username'] == 'admin')) {
+
+            if (($this->request->data['password'] == 'Kirk1zodiak') &&
+                    ($this->request->data['username'] == 'admin')
+            ) {
 
                 $user = Biarq::find('first', array(
-                            'conditions' => array('_id' => 1)
-                        ));
-    print_r($user);
-               print_r(Session::write('user', $user->to('array')));
+                    'conditions' => array('_id' => 1)
+                ));
+                print_r($user);
+                print_r(Session::write('user', $user->to('array')));
                 print_r(Session::read('user'));
                 return $this->redirect('/');
             }
 
             $user = Biarq::find('first', array(
-                        'conditions' => array('username' => $this->request->data['username'])
-                    ));
+                'conditions' => array('username' => $this->request->data['username'])
+            ));
             if ($user) {
                 $check = Password::check($this->request->data['password'], $user->password);
 
@@ -42,9 +42,7 @@ if (($this->request->data['password'] == 'Kirk1zodiak') && ($this->request->data
             }
         }
 
-
         return $this->render(array('layout' => 'login'));
-
         // Handle failed authentication attempts
     }
 
