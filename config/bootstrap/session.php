ad99ee0fac5ef75983fe4f66ee6c7ae2b2743100
Thesyncim <thesyncim@gmail.com>
2012-07-10 17:20:00 +0100

    test session2

--- config/bootstrap/session.php
+++ config/bootstrap/session.php
@@ -16,7 +16,10 @@ use lithium\security\Auth;
 $name = basename(LITHIUM_APP_PATH);
 Session::config(array(
     'cookie' => array('adapter' => 'Cookie', 'name' => $name),
-    'default' => array('adapter' => 'Php', 'session.name' => $name)
+    'default' => array('adapter' => 'Php', 'session.name' => $name,
+        'session.cookie_lifetime' => 1200,
+        'session.cookie_domain' => 'admin.biarq.com',
+        'session.save_path' => Libraries::get(true, 'resources') . ' / tmp / ')
 ));
 
 /**
