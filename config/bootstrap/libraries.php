89f381e2e92f72385e7d604ddb99d86ad8acaa77
Thesyncim <thesyncim@gmail.com>
2012-07-11 12:09:47 +0100

    config Imagine lib

--- config/bootstrap/libraries.php
+++ config/bootstrap/libraries.php
@@ -126,5 +126,6 @@ Libraries::add('li3_searchable');
 
 Libraries::add('li3_quality');
 Libraries::add('Imagine', array('path' => LITHIUM_APP_PATH . '/libraries/Imagine/lib'));
+var_dump(interface_exists('Imagine\Image\ImageInterface'));
 
 ?>
