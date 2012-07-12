95833d72fb6db9d4cfdc4d5711fb0ceb0e381adc
Thesyncim <thesyncim@gmail.com>
2012-07-12 13:11:54 +0100

    config Imagine lib

--- webroot/js/tiny_mce/plugins/imagemanager/config.php
+++ webroot/js/tiny_mce/plugins/imagemanager/config.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once($basepath . "ImageManager/ImageManagerPlugin.php");
 
 // * * * * ImageManager config
@@ -34,7 +33,8 @@ $mcImageManagerConfig['createdir.allow_override'] = "*";
 // General filesystem options
 $mcImageManagerConfig['filesystem'] = "Moxiecode_LocalFileImpl";
 $mcImageManagerConfig['filesystem.path'] = ''; // absolute or relative from this script path, optional.
-$mcImageManagerConfig['filesystem.rootpath'] = '/var/www/admin.biarq.com/webroot/img/paginas/'; // absolute or relative from this script path, required.
+$mcImageManagerConfig['filesystem.rootpath'] = LITHIUM_APP_PATH .
+        'webroot/img/paginas/'; // absolute or      relative from this script path, required.
 $mcImageManagerConfig['filesystem.datefmt'] = "Y-m-d H:i";
 $mcImageManagerConfig['filesystem.include_directory_pattern'] = '';
 $mcImageManagerConfig['filesystem.exclude_directory_pattern'] = '/^mcith$/i';
