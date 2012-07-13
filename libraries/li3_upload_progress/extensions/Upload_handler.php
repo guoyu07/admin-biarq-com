9595c0fc3ebcdfa103af4d63948d4a001c96870b
Thesyncim <thesyncim@gmail.com>
2012-07-13 21:21:01 +0100

    change file name

--- libraries/li3_upload_progress/extensions/Upload_handler.php
+++ libraries/li3_upload_progress/extensions/Upload_handler.php
@@ -69,7 +69,7 @@ class Upload_handler extends \lithium\core\Object {
     protected function get_file_object($file_name) {
         $file_path = $this->options['upload_dir'] . $file_name;
         if (is_file($file_path) && $file_name[0] !== '.') {
-            $file = new stdClass();
+            $file = new \stdClass();
             $file->name = $file_name;
             $file->size = filesize($file_path);
             $file->url = $this->options['upload_url'] . rawurlencode($file->name);
