bb72782b9365bbc17f401d43c152e6d68669ffe0
Thesyncim <thesyncim@gmail.com>
2012-07-13 11:59:16 +0100

    adapt upload handler

--- controllers/ProjectosController.php
+++ controllers/ProjectosController.php
@@ -293,6 +293,7 @@ class ProjectosController extends \lithium\action\Controller {
         header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size');
 
         print_r($this->request->get('http:method'));
+        print_r($_SERVER['REQUEST_METHOD']);
         switch ($_SERVER['REQUEST_METHOD']) {
             case 'OPTIONS':
                 break;
