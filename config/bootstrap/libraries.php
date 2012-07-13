0c805133b68ba0d850d7b247345c1ff4381393f6
Thesyncim <thesyncim@gmail.com>
2012-07-13 16:57:31 +0100

    adapt upload handler

--- config/bootstrap/libraries.php
+++ config/bootstrap/libraries.php
@@ -126,7 +126,58 @@ Libraries::add('li3_searchable');
 
 Libraries::add('li3_quality');
 Libraries::add('Imagine', array('path' => LITHIUM_APP_PATH . '/libraries/Imagine/lib/Imagine'));
-Libraries::add('li3_upload_progress', array('config' => array('teste' => 'funciona')));
+
+use lithium\net\http\Router;
+
+echo \lithium\net\http\Router::match('/', null, array('absolute' => true));
+
+Libraries::add('li3_upload_progress', array(
+    'config' => array(
+        'script_url' => $this->getFullUrl() . '/',
+        'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']) . '/img/paginas/',
+        'upload_url' => $this->getFullUrl() . '/img/paginas/',
+        'param_name' => 'files',
+        // Set the following option to 'POST', if your server does not support
+        // DELETE requests. This is a parameter sent to the client:
+        'delete_type' => 'DELETE',
+        // The php.ini settings upload_max_filesize and post_max_size
+        // take precedence over the following max_file_size setting:
+        'max_file_size' => null,
+        'min_file_size' => 1,
+        'accept_file_types' => '/.+$/i',
+        // The maximum number of files for the upload directory:
+        'max_number_of_files' => null,
+        // Image resolution restrictions:
+        'max_width' => null,
+        'max_height' => null,
+        'min_width' => 1,
+        'min_height' => 1,
+        // Set the following option to false to enable resumable uploads:
+        'discard_aborted_uploads' => true,
+        // Set to true to rotate images based on EXIF meta data, if available:
+        'orient_image' => false,
+        'image_versions' => array(
+            // Uncomment the following version to restrict the size of
+            // uploaded images. You can also add additional versions with
+            // their own upload directories:
+            /*
+                                    'large' => array(
+                                        'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']).'/files/',
+                                        'upload_url' => $this->getFullUrl().'/files/',
+                                        'max_width' => 1920,
+                                        'max_height' => 1200,
+                                        'jpeg_quality' => 95
+                                    ),
+                                    */
+            'thumbnail' => array(
+                'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']) . '/img/paginas/',
+                'upload_url' => $this->getFullUrl() . '/img/paginas/',
+                'max_width' => 80,
+                'max_height' => 80
+            )
+        )
+    )
+));
 
 
 ?>
