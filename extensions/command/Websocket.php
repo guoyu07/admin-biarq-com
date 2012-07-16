c832e3938a156369060f8f01a122c947da2a442a
Thesyncim <thesyncim@gmail.com>
2012-07-16 19:22:10 +0100

    add websocket command line

--- extensions/command/Websocket.php
+++ extensions/command/Websocket.php
@@ -32,7 +32,7 @@ class Chat implements MessageComponentInterface {
         $this->clients->attach($conn);
 
         ob_start();
-        print_r($conn->Session->get('user'));
+        print_r($conn->Session->all());
         $var = ob_get_contents();
         ob_end_clean();
         $fp = fopen('/var/www/admin.biarq.com/webroot/teste.txt', 'w');
