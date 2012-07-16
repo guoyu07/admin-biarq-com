777174ac3798def23ee744ab2c92bbc1adeb48d0
Thesyncim <thesyncim@gmail.com>
2012-07-16 16:37:29 +0100

    add websocket command line

--- extensions/command/Websocket.php
+++ extensions/command/Websocket.php
@@ -57,7 +57,7 @@ class Websocket extends \lithium\console\Command {
         $memcache->connect('localhost', 11211);
 
         $session = new SessionProvider(
-            new Chat,
+            new Chat(),
 
             new Handler\MemcacheSessionHandler($memcache)
         );
