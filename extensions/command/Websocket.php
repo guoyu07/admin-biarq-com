6e950b4e482a5cc26f59f1dec2e2970d70a6cf23
Thesyncim <thesyncim@gmail.com>
2012-07-16 18:04:05 +0100

    add websocket command line

--- extensions/command/Websocket.php
+++ extensions/command/Websocket.php
@@ -32,7 +32,7 @@ class Chat implements MessageComponentInterface {
         $this->clients->attach($conn);
 
         //var_dump($conn->session->get('user'));
-        var_dump($conn->$Session);
+        print_r($conn);
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
