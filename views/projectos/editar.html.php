2c4a54ce99fd5fdc9fc0641955ab2d8633778a5e
Thesyncim <thesyncim@gmail.com>
2012-07-17 20:06:42 +0100

    add websocket command line

--- views/projectos/editar.html.php
+++ views/projectos/editar.html.php
@@ -77,7 +77,6 @@ $this->html->style(array('imageselect', 'ui-darkness/jquery-ui-1.8.16.custom'))
 
 
 
-
 <!-- Load TinyMCE -->
 <script type="text/javascript" src="http://admin.biarq.com/js/tiny_mce/jquery.tinymce.js"></script>
 <script type="text/javascript" src="http://admin.biarq.com/js/imageselect.js"></script>
@@ -413,213 +412,85 @@ $this->html->style(array('imageselect', 'ui-darkness/jquery-ui-1.8.16.custom'))
 <script type="text/javascript" src="/js/web-socket-js/web_socket.js"></script>
 
 <script type="text/javascript">
-Base64.encode()
-var Base64 = {
-// private property
-    _keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-// public method for encoding
-    encode:function (input) {
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        input = Base64._utf8_encode(input);
-
-        while (i < input.length) {
-
-            chr1 = input.charCodeAt(i++);
-            chr2 = input.charCodeAt(i++);
-            chr3 = input.charCodeAt(i++);
-
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-
-            if (isNaN(chr2)) {
-                enc3 = enc4 = 64;
-            } else {
-                if (isNaN(chr3)) {
-                    enc4 = 64;
-                }
-            }
-
-            output = output +
-                    Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
-                    Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
-
+    $('#fileupload').fileupload({
+        drop:function (e, data) {
+            $.each(data.files, function (index, file) {
+                alert('Dropped file: ' + file.name);
+            });
+        },
+        change:function (e, data) {
+            $.each(data.files, function (index, file) {
+                alert('Selected file: ' + file.name);
+            });
         }
+    });
 
-        return output;
-    },
+    // Set URL of your WebSocketMain.swf here:
+    WEB_SOCKET_SWF_LOCATION = "WebSocketMain.swf";
+    // Set this to dump debug message from Flash to console.log:
+    WEB_SOCKET_DEBUG = true;
 
-// public method for decoding
-    decode:function (input) {
-        var output = "";
-        var chr1, chr2, chr3;
-        var enc1, enc2, enc3, enc4;
-        var i = 0;
+    // Everything below is the same as using standard WebSocket.
 
-        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    var ws;
 
-        while (i < input.length) {
+    function init() {
 
-            enc1 = Base64._keyStr.indexOf(input.charAt(i++));
-            enc2 = Base64._keyStr.indexOf(input.charAt(i++));
-            enc3 = Base64._keyStr.indexOf(input.charAt(i++));
-            enc4 = Base64._keyStr.indexOf(input.charAt(i++));
+        // Connect to Web Socket.
+        // Change host/port here to your own Web Socket server.
+        ws = new WebSocket("ws://admin.biarq.com:8000/chat");
 
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
+        // Set event handlers.
+        ws.onopen = function () {
+            output("onopen");
+        };
+        ws.onmessage = function (e) {
+            // e.data contains received string.
+            output("onmessage: " + e.data);
+            if (e.data == 'div updated') {
+                $.getJSON('http://admin.biarq.com/projectos/editar/<?=$projectos->_id?>.json',
+                        function (data) {
+                            console.log(data);
+                            document.getElementById("result").innerHTML = tmpl("tmpl-demo", data);
 
-            output = output + String.fromCharCode(chr1);
+                        });
+                console.log("works");
+                $("#imagens").empty();
 
-            if (enc3 != 64) {
-                output = output + String.fromCharCode(chr2);
             }
-            if (enc4 != 64) {
-                output = output + String.fromCharCode(chr3);
-            }
+        };
+        ws.onclose = function () {
 
-        }
+            output("onclose");
+            setTimeout(function () {
+                ws = new WebSocket("ws://localhost:8000/chat");
+            }, 5000);
+        };
+        ws.onerror = function () {
+            output("onerror");
+        };
 
-        output = Base64._utf8_decode(output);
-
-        return output;
-
-    },
-
-// private method for UTF-8 encoding
-    _utf8_encode:function (string) {
-        string = string.replace(/\r\n/g, "\n");
-        var utftext = "";
-
-        for (var n = 0; n < string.length; n++) {
-
-            var c = string.charCodeAt(n);
-
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            }
-            else {
-                if ((c > 127) && (c < 2048)) {
-                    utftext += String.fromCharCode((c >> 6) | 192);
-                    utftext += String.fromCharCode((c & 63) | 128);
-                }
-                else {
-                    utftext += String.fromCharCode((c >> 12) | 224);
-                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                    utftext += String.fromCharCode((c & 63) | 128);
-                }
-            }
-
-        }
-
-        return utftext;
-    },
-
-// private method for UTF-8 decoding
-    _utf8_decode:function (utftext) {
-        var string = "";
-        var i = 0;
-        var c = c1 = c2 = 0;
-
-        while (i < utftext.length) {
-
-            c = utftext.charCodeAt(i);
-
-            if (c < 128) {
-                string += String.fromCharCode(c);
-                i++;
-            }
-            else {
-                if ((c > 191) && (c < 224)) {
-                    c2 = utftext.charCodeAt(i + 1);
-                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                    i += 2;
-                }
-                else {
-                    c2 = utftext.charCodeAt(i + 1);
-                    c3 = utftext.charCodeAt(i + 2);
-                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) |
-                            (c3 & 63));
-                    i += 3;
-                }
-            }
-
-        }
-        return string;
     }
-}
 
-// Set URL of your WebSocketMain.swf here:
-WEB_SOCKET_SWF_LOCATION = "WebSocketMain.swf";
-// Set this to dump debug message from Flash to console.log:
-WEB_SOCKET_DEBUG = true;
+    function onSubmit() {
+        var input = document.getElementById("input");
+        // You can send message to the Web Socket using ws.send.
+        ws.send(input.value);
+        output("send: " + input.value);
+        input.value = "";
+        input.focus();
+    }
 
-// Everything below is the same as using standard WebSocket.
+    function onCloseClick() {
+        ws.close();
+    }
 
-var ws;
-
-function init() {
-
-    // Connect to Web Socket.
-    // Change host/port here to your own Web Socket server.
-    ws = new WebSocket("ws://admin.biarq.com:8000/chat");
-
-    // Set event handlers.
-    ws.onopen = function () {
-        output("onopen");
-    };
-    ws.onmessage = function (e) {
-        // e.data contains received string.
-        output("onmessage: " + e.data);
-        if (e.data == 'div updated') {
-            $.getJSON('http://admin.biarq.com/projectos/editar/<?=$projectos->_id?>.json',
-                    function (data) {
-                        console.log(data);
-                        document.getElementById("result").innerHTML = tmpl("tmpl-demo", data);
-
-                    });
-            console.log("works");
-            $("#imagens").empty();
-
-        }
-    };
-    ws.onclose = function () {
-
-        output("onclose");
-        setTimeout(function () {
-            ws = new WebSocket("ws://localhost:8000/chat");
-        }, 5000);
-    };
-    ws.onerror = function () {
-        output("onerror");
-    };
-
-}
-
-function onSubmit() {
-    var input = document.getElementById("input");
-    // You can send message to the Web Socket using ws.send.
-    ws.send(input.value);
-    output("send: " + input.value);
-    input.value = "";
-    input.focus();
-}
-
-function onCloseClick() {
-    ws.close();
-}
-
-function output(str) {
-    var log = document.getElementById("log");
-    var escaped = str.replace(/&/, "&amp;").replace(/</, "&lt;").
-            replace(/>/, "&gt;").replace(/"/, "&quot;"); // "
-    log.innerHTML = escaped + "<br>" + log.innerHTML;
-}
+    function output(str) {
+        var log = document.getElementById("log");
+        var escaped = str.replace(/&/, "&amp;").replace(/</, "&lt;").
+                replace(/>/, "&gt;").replace(/"/, "&quot;"); // "
+        log.innerHTML = escaped + "<br>" + log.innerHTML;
+    }
 
 </script>
 
