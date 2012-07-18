0f81a4fc0ccb05dca7db5644472012bfd75e4cc9
Thesyncim <thesyncim@gmail.com>
2012-07-18 14:40:58 +0100

    add websocket command line

--- views/projectos/editar.html.php
+++ views/projectos/editar.html.php
@@ -185,7 +185,7 @@ $this->html->style(array('imageselect', 'ui-darkness/jquery-ui-1.8.16.custom'))
     <script type="text/javascript">
 
         $(window).load(
-                $("select[name='fotoprincipal']").ImageSelect({dropdownWidth:125});
+                $("select[name='fotoprincipal']").ImageSelect({dropdownWidth:125})
         )
 
 
