b5656191c1998d0614d67ea5356fdc0876b0f3f5
Thesyncim <thesyncim@gmail.com>
2012-07-26 18:40:31 +0100

    New changelist

--- controllers/ProjectosController.php
+++ controllers/ProjectosController.php
@@ -273,7 +273,7 @@ class ProjectosController extends \lithium\action\Controller {
 
     public function teste() {
 
-        foreach(glob(Libraries::get(true, 'path') . '/webroot/img/projectos/pequenas/*.jpg') as
+        foreach(glob(Libraries::get(true, 'path') . '/webroot/img/projectos/galeria/*.jpg') as
                 $path) {
 
             if (strpos($path, 'thumb') !== false) {
