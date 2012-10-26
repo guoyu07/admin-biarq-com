33d79026754694137f53e1c55dc3ff939fdddba4
syncim <thesyncim@gmail.com>
2012-10-26 12:53:01 +0100

    recover big images

--- extensions/command/Install.php
+++ extensions/command/Install.php
@@ -8,6 +8,8 @@
 
 namespace app\extensions\command;
 
+use app\models\Projectos;
+
 class Install extends \lithium\console\Command {
 
 	
@@ -20,13 +22,52 @@ class Install extends \lithium\console\Command {
 	}
 
 	public function run() {
-		$this->header("Installing Biarq Admin site");
+		/*$this->header("Installing Biarq Admin site");
 
 		$cmd = "find {$this->webroot}/img/. -type d -exec chmod 777 {} \;";
 
 		$this->out("Executing: $cmd"); 
 		
-		system($cmd);
+		system($cmd);*/
+
+        $imagine = new \Imagine\Gmagick\Imagine();
+        $size = new \Imagine\Image\Box(635, 381);
+        $mode = \Imagine\Image\ImageInterface::THUMBNAIL_INSET;
+
+
+        $projectos = Projectos::find('all', array(
+
+                'order' => array('ordem' => 'ASC'),
+                'fields' => 'foto', "limit" => 3, "page" => 4)
+        );
+
+        $fotos = [];
+        foreach ($projectos as $projecto) {
+            foreach ($projecto->to('array') as $fotoarray) {
+                foreach ($fotoarray as $foto) {
+                    array_push($fotos, $foto);
+
+                }
+            }
+        }
+        $cont = 0;
+
+
+        foreach (glob(LITHIUM_APP_PATH . "/webroot/img/projectos/*.jpg") as $srcimg) {
+
+            $imgname = substr($srcimg, -20);
+            if (in_array($imgname, $fotos)) {
+                $new_file_path = Libraries::get(true, 'path') . '/webroot/img/projectos/grandes/' . $imgname;
+                $success = $imagine->open($srcimg)
+                    ->resize($size, $mode)
+                    ->save($new_file_path);
+                if ($success)
+                    echo "done $imgname /n";
+
+            }
+
+        }
+        echo $cont;
 	}
 
 }
